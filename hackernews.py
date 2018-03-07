d1daf95d1c45c79acdefe15476fab744da388f03
abhi-jha <abhi_jha1995@rediffmail.com>
2018-03-08 00:39:02 +0530

    get hackernews stories into sql database

--- hackernews.py
+++ hackernews.py
@@ -12,7 +12,7 @@ import requests
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler('hackernews_temp.log')
+fh = logging.FileHandler('hackernews.log')
 fh.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
